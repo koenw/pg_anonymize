69fb6ffc0583c8eb781b5bbf022f983e12140323
Koen Wilde <koen@chillheid.nl>
2020-01-20 14:11:34 +0100

    Re-order code for improved readability

--- src/main.rs
+++ src/main.rs
@@ -7,29 +7,17 @@ use std::collections::HashMap;
 mod strategy;
 use strategy::Strategy;
 
-#[derive(Debug, StructOpt)]
-#[structopt(name = "pg_anonymize", about = "Anonymize PostgreSQL COPY output")]
-struct Opts {
-#[structopt()]
-    /// The table we're COPY-ing
-    table: String,
+fn main() {
+    let opts = Opts::from_args();
+    let config_raw = std::fs::read_to_string("config.yaml").expect("Failed to read config.yaml");
+    let config: Config = serde_yaml::from_str(&config_raw).expect("Failed to decode config.yaml");
+    let stdin = std::io::stdin();
+
+    for row in stdin.lock().lines() {
+        let row = row.unwrap();
+        process_row(&config.tables, &row, &opts.table);
+    }
 }
-
-#[derive(Deserialize)]
-struct Config {
-    tables: HashMap<TableName, TableConfig>,
-}
-
-type TableConfig = HashMap<ColumnName, ColumnConfig>;
-
-#[derive(Deserialize)]
-struct ColumnConfig {
-    column: usize,
-    strategy: Strategy,
-}
-
-type ColumnName = String;
-type TableName = String;
 
 fn process_row<'a>(config: &HashMap<TableName, TableConfig>, row: &'a str, table: &str) {
     // If we don't have a translation config for a table, we just print the row unchanged.
@@ -52,14 +40,33 @@ fn process_row<'a>(config: &HashMap<TableName, TableConfig>, row: &'a str, table
     println!("{}", new_row.join("\t"));
 }
 
-fn main() {
-    let opts = Opts::from_args();
-    let config_raw = std::fs::read_to_string("config.yaml").expect("Failed to read config.yaml");
-    let config: Config = serde_yaml::from_str(&config_raw).expect("Failed to decode config.yaml");
-    let stdin = std::io::stdin();
+/// `Opts` contains the command line arguments passed to our program.
+#[derive(Debug, StructOpt)]
+#[structopt(name = "pg_anonymize", about = "Anonymize PostgreSQL COPY output")]
+struct Opts {
+#[structopt()]
+    /// The table we're COPY-ing
+    table: String,
+}
 
-    for row in stdin.lock().lines() {
-        let row = row.unwrap();
-        process_row(&config.tables, &row, &opts.table);
-    }
+type TableName = String;
+
+/// `Config` represents our configuration file.
+#[derive(Deserialize)]
+struct Config {
+    /// tables contains the configuration for our tables, indexed by TableName.
+    tables: HashMap<TableName, TableConfig>,
+}
+
+type ColumnName = String;
+type TableConfig = HashMap<ColumnName, ColumnConfig>;
+
+/// `ColumnConfig` represents the actual anonymization configuration for a column.
+#[derive(Deserialize)]
+struct ColumnConfig {
+    /// `column` is used to identify the column. Unfortunately we can't match on name; instead we
+    /// need to know the index (relative order) of the column relative to the other columns.
+    column: usize,
+    /// `Strategy` determines the strategy used to generate data for this column.
+    strategy: Strategy,
 }
